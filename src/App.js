4156785ed614135b43aff625d1e29f2c325ff519
nehalmehta36 <nehalmehta36@gmail.com>
2019-08-31 17:19:20 +0530

    passed heading to header component as prop

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import {render} from "react-dom"
 function App() {
    let subscribers = [
       {
+         id: "1",
          name:"nehal",
          phone:"9958961516"
       },
       {
+         id:"2",
          name: 'rita',
          phone: "885152623"
       }
@@ -19,20 +21,22 @@ function App() {
 //   render() {
   return (
     <fragment>
-      <Header />
-      <button id="btn">ADD</button><br />
+      <Header heading = "Phone Directory"/>
+      <button className="btn">ADD</button><br />
       <div className= "grid-container heading-container">
       <span className= "nameHeading">NAME</span>
       <span className="phoneHeading">PHONE</span>
       </div>
       {
          subscribers.map(sub =>{
-            return <div className="grid-container">
+            return <div className="grid-container" key={sub.id}>
                <span className="grid-item">{sub.name}</span>
                <span className="grid-item">{sub.phone}</span>
+               <button className = "grid-item  delBtn ">Delete</button>
             </div>
          })
       }
+      
     </fragment>
   );
 //   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import './Header.css'
 
-const Header = function(){
+const Header = function(props){
     //const head = {textAlign:'center',padding:20,background:'black',textTransform:'uppercase',color:'white' };
     return(
         
         <div className= "header">
-        Phone Directory
+            {props.heading}
         </div>
     )
 }
